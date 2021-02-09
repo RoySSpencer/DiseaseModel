44dd3a6af5c9b22831b20eddb7de05e60d3264d6
RoySSpencer <spencersroy@gmail.com>
2021-02-08 22:49:59 -0500

    extra stuff

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import networkx as nx
 import random
 import matplotlib.pyplot as plt
 
-def main(plot=False, load=False, prnt_list=False, start_percent=5, infect_percent=15, days_infected=3, nodes=100, type="watts", draw="circular"):
+def main(plot=False, load=False, prnt_list=False, start_percent=5, infect_percent=15,
+        days_infected=3, nodes=100, type="watts", draw="circular", vaccinate_percent=0, vaccine_affectiveness=0):
     num_days = list()
+    num_healthy = list()
     for i in range(0, 10):
         if load:
             graphs = LoadFile()
@@ -11,6 +13,7 @@ def main(plot=False, load=False, prnt_list=False, start_percent=5, infect_percen
             graphs = GetRandomGraph(nodes, type)
         print("Clustering Coefficient: " + str(nx.clustering(graphs[0])))
         G = StartInfection(graphs[0], start_percent, days_infected)
+        G = Vaccinate(graphs[0], vaccinate_percent)
         color_map = MakeColorMap(G)
 
 
@@ -29,7 +32,7 @@ def main(plot=False, load=False, prnt_list=False, start_percent=5, infect_percen
         # for i in range(0, 10):
         i = 0
         while "Infected" in statuses:
-            G = SpreadInfection(G, infect_percent)
+            G = SpreadInfection(G, infect_percent,vaccine_affectiveness)
             # color_map = MakeColorMap(G)
             # nx.draw(G, node_color=color_map, with_labels=True, font_weight='bold')
             # plt.show()
@@ -50,11 +53,21 @@ def main(plot=False, load=False, prnt_list=False, start_percent=5, infect_percen
                     nx.draw(G, node_color=color_map, with_labels=True, font_weight='bold')
                 plt.show()
         num_days.append(i)
+        num_healthy.append(len(statuses['Healthy']))
     print(num_days)
+    print(num_healthy)
 
 
+def Vaccinate(G, vaccinate_percent):
+    for node_data in G.nodes.data():
+        node = G.nodes[node_data[0]]
+        if random.randint(0, 100) < vaccinate_percent:
+            node['vaccinated'] = True
+        else:
+            node['vaccinated'] = False
+    return G
 
-def SpreadInfection(G, infect_percent):
+def SpreadInfection(G, infect_percent, vaccine_affectiveness):
     for edges in G.edges:
         if (G.nodes[edges[0]]["status"] == "Healthy" and G.nodes[edges[1]]["status"] == "Infected") or (G.nodes[edges[0]]["status"] == "Infected" and G.nodes[edges[1]]["status"] == "Healthy"):
             node = edges[0]
@@ -62,7 +75,8 @@ def SpreadInfection(G, infect_percent):
                 node = edges[1]
 
             if random.randint(0, 100) < infect_percent:
-                G.nodes[node]["status"] = "Newly Infected"
+                if not G.nodes[node]["vaccinated"] or random.randint(0, 100) > vaccine_affectiveness:
+                    G.nodes[node]["status"] = "Newly Infected"
 
         # print(edges[0] + ":" + G.nodes[edges[0]]["status"])
         # print(edges[1] + ":" + G.nodes[edges[1]]["status"])
@@ -118,7 +132,7 @@ def GetRandomGraph(nodes, type):
     if (type == "random"):
         return [nx.gnm_random_graph(nodes, 350)]
     elif (type == "watts"):
-        return [nx.connected_watts_strogatz_graph(nodes, 5, .3)]
+        return [nx.connected_watts_strogatz_graph(nodes, 10, .3)]
     return [nx.connected_watts_strogatz_graph(nodes, 5, .3)]
 
 def LoadFile():
